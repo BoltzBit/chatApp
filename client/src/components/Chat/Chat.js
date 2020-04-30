65a1e06855d9c6ed5c967c475db883708e403c94
BoltzBit <mateus_desktop@outlook.com>
2020-04-30 16:00:45 -0300

    chatapp

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,15 +2,69 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const Chat = ({ location }) => {
-    useEffect(() => {
-        const data = queryString.parse(location.search);
-        
-        console.log(location.search);
-        console.log(data);
-    });
+import InfoBar from '../InfoBar/InfoBar';
 
-    return <h1>Chat</h1>;
+import './Chat.css'
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [ name, setName ] = useState('');
+    const [ room, setRoom ] = useState('');
+    const [ message, setMessage ] = useState('');
+    const [ messages, setMessages ] = useState([]);
+    const ENDPOINT = 'localhost:5000';
+
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+        
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room}, () => {
+        });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+
+    },[ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([ ...messages, message ]);
+        });
+    }, [ messages ]);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
+    return (
+        <div className="outerContainer">
+            <div className="container">
+
+                <InfoBar />
+
+                {/*<input
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null }
+                />*/}
+            </div>
+        </div>
+    );
 };
 
 export default Chat;
---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InfoBar = () => {
+    return <h1>InfoBar</h1>;
+}
+
+export default InfoBar;
